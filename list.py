7889a2b50407543f256ee3e28a49ced035f30a24
kuldeep114 <eleckuldeep@gmail.com>
2017-06-19 11:27:58 +0530

    testing

---
+++ list.py
@@ -0,0 +1,11 @@
+fruit_list = ['banana','mango','apple','lichi']
+print 'i have', len(fruit_list),'fruit to purchase'
+print 'these items are',
+for item in(fruit_list):
+ print item,
+print 'i have to purchase one more fruit'
+fruit_list.append('grapes')
+print 'now i have to purchase',len(fruit_list),'fruits',
+
+for item in(fruit_list):
+ print item,
